904e3f7a7eebaa3c3da65a52a59ff9bbe275e75f
Bruna Gomes <bruna.ckgomes@gmail.com>
2025-10-10 07:20:52 -0300

    Adicionando stages ao teste

--- tests/login.test.js
+++ tests/login.test.js
@@ -4,8 +4,15 @@ import { sleep, check } from 'k6';
 
 export const options = {
     //iterations: 1,
-    vus: 10,
-    duration: '30s',
+   // vus: 10,
+  //  duration: '30s',
+    stages: [
+        { duration: '10s', target: 10 },
+        { duration: '20s', target: 10 },
+        { duration: '10s', target: 30 },
+        { duration: '20s', target: 30 },
+        { duration: '20s', target: 0 },
+    ],
     thresholds: {
         http_req_duration: ['p(90)<3000', 'max<5000'], // 90% das requisições devem ser respondidas em menos de 500ms
         http_req_failed: ['rate<0.01'] // taxa de erro deve ser menor que 1%
